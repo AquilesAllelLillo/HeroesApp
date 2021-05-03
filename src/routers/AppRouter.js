90ad756e64cc66b23960cce4477131a10de6b543
AquilesAllelLillo <aquilesallellillo@gmail.com>
2021-05-03 13:23:13 -0400

    Mejora manejo rutas públicas y privadas, junto con mejora manejo login

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+
+import { AuthContext } from "../auth/authContext";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 
 export const AppRouter = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact path="/login" component={LoginScreen}>
-            {/* <About /> */}
-          </Route>
-          <Route path="/" component={DashboardRoutes}>
-            {/* <Users /> */}
-          </Route>
+          <PublicRoute
+            exact
+            path="/login"
+            component={LoginScreen}
+            isAuthenticated={user.logged}
+          />
+          <PrivateRoute
+            path="/"
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </Router>
